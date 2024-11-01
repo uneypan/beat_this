b7e247a151a551032fdacd8b094c1c8e9ea80939
uneypan <zhihan.scut@gmail.com>
2024-11-01 17:12:23 +0800

    replace path '\\'  with '/' for windows

--- beat_this/dataset/dataset.py
+++ beat_this/dataset/dataset.py
@@ -145,7 +145,7 @@ class BeatTrackingDataset(Dataset):
 
     def _get_spect(self, item):
         try:
-            dataset, filename = str(item["spect_path"]).split("/", 1)
+            dataset, filename = str(item["spect_path"]).replace("\\", "/").split("/", 1)
             spect = self.spects[dataset][filename[:-4]]
         except KeyError:
             spect = np.load(self.spect_basepath / item["spect_path"], mmap_mode="r")
